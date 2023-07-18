8167b1b3a158c94da88426bbe01c5d4adb88a2bf
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-07-18 19:22:36 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -253,8 +253,8 @@ export const App = () => {
             <input
               type="text"
               name="LastName"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               placeholder="LastName"
               onChange={handleChangeLastName}
