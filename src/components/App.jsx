ebdfdb6d8b2f475bbd13a7ad5e27b7e6d9045eaf
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-07-18 19:25:14 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -264,7 +264,7 @@ export const App = () => {
             <input
               type="email"
               name="email"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               placeholder="email"
